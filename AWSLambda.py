63eb381cf4654d6ff767d35cb14d90b72b9df13d
hakngrow <56512250+hakngrow@users.noreply.github.com>
2020-02-11 15:14:15 +0800

    20200211 1452

--- AWSLambda.py
+++ AWSLambda.py
@@ -3,10 +3,19 @@ import json
 
 import utils.aws.AWSDB as db
 
+def lambda_handler(event, context):
 
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
+
+'''
 posts = db.get_reddit_posts('worldnews')
 
 for post in posts:
     pp.pprint(post.__dict__, indent=4)
-
+'''
 
